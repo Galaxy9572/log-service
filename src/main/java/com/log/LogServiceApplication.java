7f3b7a46d4ef7313106cd10f5e0144374375dd6d
Galaxy9572 <abcd957266>
2020-06-20 10:57:31 +0800

    init

---
+++ src/main/java/com/log/LogServiceApplication.java
@@ -0,0 +1,16 @@
+package com.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 廖俊瑶
+ */
+@SpringBootApplication
+public class LogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogServiceApplication.class, args);
+    }
+
+}
